2514ec6ae4c37dd07e63517b7e51440a74e9ebe7
Danil Chaban <krigerblood@gmail.com>
2023-01-15 20:34:36 +0200

    Add todos code

---
+++ angular-practice/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import {TodosModule} from "./todos/todos.module";
+import {AppRoutingModule} from "./app-routing.module";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    TodosModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
